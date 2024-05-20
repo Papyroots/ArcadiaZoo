34dfa4d9f917277a06dcbad805461fd8cf14b36d
Arona <papyroots@gmail.com>
2024-05-20 21:28:34 +0200

    install bootstrap

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,19 +4,19 @@ import Route from "./Route.js";
 // Déclaration des routes
 export const allRoutes = [
   // Route pour la page d'accueil
-  new Route("/", "Accueil", "/pages/home.html"),
+  new Route("/", "Accueil", "./pages/home.html"),
 
   // Route pour la page des services
-  new Route("/galerie", "Services", "/pages/services.html"),
+  new Route("/services", "Services", "./pages/services.html"),
 
   // Route pour la page des habitats
-  new Route("/tarifs", "Habitats", "/pages/habitats.html"),
+  new Route("/habitats", "Habitats", "./pages/habitats.html"),
 
   // Route pour la page de connexion
-  new Route("/connexion", "Connexion", "/pages/connexion.html"),
+  new Route("/connexion", "Connexion", "./pages/connexion.html"),
 
   // Route pour la page de contact
-  new Route("/contact", "Contact", "/pages/contact.html"),
+  new Route("/contact", "Contact", "./pages/contact.html"),
 ];
 
 // Nom du site web
